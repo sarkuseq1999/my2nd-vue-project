5d50b89eae576a6e851c21c2a660f23de67264b0
sarkuseq1999 <yvonnemo@gmail.com>
2025-02-19 16:37:21 -0800

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <div class="flex justify-center h-50 items-center bg-gray-200 space-x-4 m-4 rounded-lg p-4">
+    <div class="text-purple-700">This is my vue world wonderful! Testing!</div>
+    <div class="text-green-700">This is going to be great!</div>
+    <div class="text-orange-700">This is what I wanted!</div>
+  </div>
+  <div class="flex h-80 justify-center items-center space-x-4 bg-green-200 rounded-lg">
+    <div class="text-blue-700">Muhahaha</div>
+    <div>I can do anything with it</div>
+    <div>This is great</div>
+  </div>
+
+  <div class="bg-blue-500 text-white text-lg p-4 md:bg-green-500 lg:bg-red-500">
+  Resize the screen to see color changes!
+</div>
+
+<div class="flex flex-col md:flex-row bg-gray-200 p-4 gap-4">
+  <div class="bg-blue-500 text-white p-6 flex-1 text-center">Item 1</div>
+  <div class="bg-green-500 text-white p-6 flex-1 text-center">Item 2</div>
+  <div class="bg-red-500 text-white p-6 flex-1 text-center">Item 3</div>
+</div>
+
+</template>
+
+<style scoped>
+
+</style>
