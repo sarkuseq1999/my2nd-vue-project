80ef163f13244da2fbae003b2074b05e0793aacf
Mo Chen <mochen@Mos-iMac.local>
2025-02-20 16:58:52 -0800

    Testing a component

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,13 @@
 <script setup>
-
-
+    import NavBar from './components/NavBar.vue'
+    
 </script>
 
 <template>
+
+<div id="app">
+        <NavBar />
+      </div>
 
   <div class="flex justify-center h-50 items-center bg-amber-400 border-s-green-300 space-x-4 m-2 rounded-lg p-4">
     <div class="text-purple-700">This is my vue world wonderful! Testing12!!!!!!!!</div>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+      <p class="text-white bg-blue-400">Login | Signup</p>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'NavBar'
+  }
+  </script>
