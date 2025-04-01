2ae693db3d3a821a3bc69bb587ac75d9dd2bb698
sarkuseq1999 <yvonnemo@gmail.com>
2025-04-01 10:37:39 -0700

    testing

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -40,17 +40,13 @@ onMounted(() => {
   </section>
 
   <!-- Bottom Section -->
-  <section class="relative h-[500px] bg-purple-400 flex items-center justify-center text-white text-4xl">
+  <section class="relative z-20 flex items-center justify-center h-[500px] bg-cover bg-center bg-no-repeat" style="background-image: url('/images/testingsection1.png');">
       Bottom Section
 
-      <!-- Cloud Shape Divider -->
-      <svg class="absolute bottom-0 left-0 w-full" viewBox="0 0 1440 320" xmlns="http://www.w3.org/2000/svg">
-        <path fill="#ffffff" d="M0,256L48,245.3C96,235,192,250,500,218.7C384,224,480,256,576,266.7C672,277,768,267,864,240C960,213,1056,171,1152,165.3C1248,160,1344,192,1392,208L1440,224L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path>
-      </svg>
   </section>
 
   <!-- 22 Years of Excellence Section -->
-  <section class="relative flex items-center justify-center h-[500px] bg-cover bg-center bg-no-repeat" style="background-image: url('/images/22yrs.jpg');">
+  <section class="relative z-10 -mt-20 flex items-center justify-center h-[500px] bg-cover bg-center bg-no-repeat" style="background-image: url('/images/22yrs.jpg');">
       
       <!-- Overlay for Readability -->
       <div class="absolute inset-0 bg-black opacity-20"></div>
